a785a7904895d60510c9344866f51046bf3e21e8
hasagiiiiii <dvant24102003@gmail.com>
2023-10-19 22:59:06 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,11 @@ import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import RootLayout from "./Layout/RootLayout";
 import "./App.css";
-import TestAPI from "./TestAPI";
 const App = () => {
   const router = createBrowserRouter([
     {
       path: "/",
       element: <RootLayout />,
-    },
-    {
-      path: "/testAPI",
-      element: <TestAPI />,
     },
   ]);
   return <RouterProvider router={router} />;

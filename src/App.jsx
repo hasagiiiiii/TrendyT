2b9d9d9a5f55c141b9db00efcd18c04db6bb2169
GreenieVH <tranhoangth1979@gmail.com>
2023-10-23 22:12:16 +0700

    viethoang layout quản lý sản phẩm

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
 import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import RootLayout from "./Layout/RootLayout";
+import AllProduct from "./component/ProductManagementLayout/AllProduct";
 import "./App.css";
+import ListAll from "./component/ProductManagementLayout/ProductListLayout/ListAll";
 const App = () => {
   const router = createBrowserRouter([
     {
       path: "/",
       element: <RootLayout />,
+      children: [
+        {
+          path: "/AllProduct",
+          element: <AllProduct/>,
+          children: [
+            {
+              path: '/AllProduct/list/all',
+              element:<ListAll/>
+            }
+          ]
+        }
+      ]
     },
   ]);
   return <RouterProvider router={router} />;
